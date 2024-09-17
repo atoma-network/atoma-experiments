54702af067aadc5a0481fc3cfd4ee247e850ec61
jorgeantonio21 <matroid@outlook.com>
2024-09-17 14:57:30 +0100

    simplify x crate code

--- x/src/main.rs
+++ x/src/main.rs
@@ -1,23 +1,18 @@
 use anyhow::Result;
 use dotenv::dotenv;
-use rag::{client::EmbeddingClient, server::start, types::TextToEmbed};
+use rag::types::TextToEmbed;
 use reqwest::Client;
 use std::{
     env,
     hash::{DefaultHasher, Hash, Hasher},
 };
-use tracing::{error, info, info_span};
+use tracing::{error, info};
 use x::note_tweet::parse_note_tweets;
 
 const INDEX_NAME: &str = "atoma-alpha-mistral";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
-
-    let span = info_span!("x");
-    let _enter = span.enter();
-
     dotenv().expect("Failed to load .env file");
     let username = env::var("USERNAME").expect("USERNAME not set");
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
@@ -25,30 +20,10 @@ async fn main() -> Result<()> {
         .ok()
         .and_then(|p| p.parse().ok())
         .unwrap_or(8081);
-    let embedding_host = env::var("EMBEDDING_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let embedding_port = env::var("EMBEDDING_PORT")
-        .ok()
-        .and_then(|p| p.parse().ok())
-        .unwrap_or(8080);
 
-    let pinecone_api_key = env::var("PINECONE_API_KEY").unwrap();
-    let pinecone_host = env::var("PINECONE_HOST").unwrap();
     let note_tweets =
         parse_note_tweets(&env::var("NOTE_TWEET_FILE").expect("NOTE_TWEET_FILE not set"))
             .expect("Failed to parse note tweets json file");
-
-    let host_clone = host.clone();
-    let _join_handle = tokio::spawn(async move {
-        let client = EmbeddingClient::new(
-            embedding_host,
-            embedding_port,
-            pinecone_api_key,
-            pinecone_host,
-        )
-        .await?;
-        start(&host_clone, port, client).await?;
-        Ok::<_, anyhow::Error>(())
-    });
 
     let client = Client::new();
     for note_tweet in note_tweets {
@@ -74,7 +49,7 @@ async fn main() -> Result<()> {
             .await
         {
             Ok(response) => {
-                info!("Succesfully embedded result: {:?}", response);
+                info!("Successfully embedded result: {:?}", response);
             }
             Err(e) => {
                 error!("Error: {:?}", e);
